1e02b22cf54f8e0896287db89c931fb415c3c08e
21-Shivansh <shivanshsingh2111@gmail.com>
2026-09-06 19:28:35 +0530

    protected route

--- react10/src/routes/AppRoutes.jsx
+++ react10/src/routes/AppRoutes.jsx
@@ -5,13 +5,18 @@ import AboutPage from '../pages/AboutPage'
 import ContactPage from '../pages/ContactPage'
 import Product from '../pages/Product'
 import CartPage from '../pages/CartPage'
+import ProtectedRoute from './ProtectedRoute'
 
 const AppRoutes = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={ < HomePage /> }/>
-        <Route path='/about' element={ < AboutPage /> }/>
+        <Route path='/about' element={ 
+          <ProtectedRoute>
+            <AboutPage />
+          </ProtectedRoute>
+         }/>
         <Route path='/contact' element={ < ContactPage /> }/>
         <Route path='/product/:id' element={ <Product/> }/>
         <Route path='/cart' element={ <CartPage/> }/>
