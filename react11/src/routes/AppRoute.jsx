16c977c4d270b8ff39f8456d52f3e2e136b50802
21-Shivansh <shivanshsingh2111@gmail.com>
2026-09-12 19:43:39 +0530

    Data routing

---
+++ react11/src/routes/AppRoute.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Home from '../pages/Home'
+import About from '../pages/About'
+import Contact from '../pages/Contact'
+import MainLayout from '../layout/MainLayout'
+
+const AppRoute = () => {
+
+    let router = createBrowserRouter([
+        {
+            path:'/',
+            element:<MainLayout/>,
+            children:[
+                {
+                    path:'',
+                    element:<Home/>
+                },
+                {
+                    path:'about',
+                    element:<About/>
+                },
+                {
+                    path:'contact',
+                    element:<Contact/>
+                },
+            ]
+        },
+    ])
+
+
+
+  return (<RouterProvider router={router}/> )
+}
+
+export default AppRoute
